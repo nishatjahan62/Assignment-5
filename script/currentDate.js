12a202407fadabe68335fa4aa64ca4e6a1fb7f94
nishatjahan62 <nishatjahanposhpa@gmail.com>
2025-03-02 14:44:18 +0600

    added daynamic current Date

---
+++ script/currentDate.js
@@ -0,0 +1,14 @@
+function dynamicDate() {
+  const currentDate = new Date();
+
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const dayName = days[currentDate.getDay()];
+
+  const month = currentDate.toLocaleDateString("default", { month: "short" });
+  const day = currentDate.getDate();
+  const year = currentDate.getFullYear();
+  document.getElementById("dayName").textContent = dayName + ",";
+  document.getElementById("date").textContent = month +  " " +  day + " " + year;
+  
+}
+window.onload = dynamicDate;
